0ac64cac53aade9791bef7f1a408abe1bddae461
marisleydis <68972262+marisleydis@users.noreply.github.com>
2023-01-04 15:28:50 +0100

    commit code

---
+++ My first Java/src/main/java/Exercices.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Exercices {
+    public static void main(String args[]) {
+        Scanner scanner =  new Scanner(System.in);
+        System.out.println("Introduce un adjetivo: ");
+        String adjective = scanner.nextLine();
+        System.out.println("Introduce un estacion: ");
+        String season = scanner.nextLine();
+        System.out.println("Cuantos cafes te has tomado? ");
+        double cafe = scanner.nextDouble();
+
+        System.out.println("On a "+adjective+" "+ season+" day, I drink a minimum of "+cafe+" cups of coffee");
+
+        boolean font =true;
+        System.out.println("Y la información es: "+font);
+
+        int raton, gato;
+        System.out.println("Introduce el numero de ratones: ");
+        raton = scanner.nextInt();
+        System.out.println("Introduce el numero de gatos: ");
+        gato = scanner.nextInt();
+        System.out.print("Restamos: ");
+        System.out.println(raton-gato);
+        System.out.print("Sumamos: ");
+        System.out.println(raton+gato);
+        System.out.print("Multiplicamos: ");
+        System.out.println(raton*gato);
+        System.out.print("Dividimos: ");
+        System.out.println(raton/gato);
+
+
+    }
+}
